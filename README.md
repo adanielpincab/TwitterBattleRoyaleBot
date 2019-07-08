dbf24dbd595d65b1d767f2d27cd0f358a0232308
Piña <adanielpincab@gmail.com>
2019-07-08 13:06:20 +0200

    Funcionalidad basica añadida Junto a una nueva licencia, el script es ahora funcional. Solo falta su implementación en Twitter.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # TwitterBattleRoyaleBot
 Bot para organizar un denominado "battle royale" en Twitter, (enfrentando de manera ficticia a varios oponentes hasta que solo queda uno).
+
+>  This Source Code Form is subject to the terms of the Mozilla Public
+>  License, v. 2.0. If a copy of the MPL was not distributed with this
+>  file, You can obtain one at http://mozilla.org/MPL/2.0/.
--- main.py
+++ main.py
@@ -1,27 +1,49 @@
-
 '''
-   Copyright 2019 @Pinneaple6
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 '''
 
+# ----------------------------------------------------------------
 from random import choice, seed
 from re import sub
+from json import loads
+from time import sleep
 # TODO: añadir la parte de interacción con Twitter
 
-# ---
-SEED = '' # Dejar en blanco para que sea totalmente aleatorio
-PEOPLE = 'example.json'
-PHRASES = 'phrases.json'
-# ---
+with open('custom/PEOPLE.txt', 'r') as f:
+    people = f.readlines()
+    people = [i.rstrip('\n') for i in people] 
+    f.close()
 
+with open('custom/PHRASES.txt', 'r') as f:
+    phrases = f.readlines()
+    phrases = [i.rstrip('\n') for i in phrases]
+    f.close()
+
+SEED = '' # Dejar en blanco para que sea totalmente aleatorio
+DELAY = 10 # Tiempo (en segundos) entre cada tweet
+# ----------------------------------------------------------------
+
+if SEED != '': 
+    seed(SEED)
+
+while True:
+    message = ''
+
+    # Primero, se elige a una víctima y se elimina del grupo de participantes
+    victim = choice(people)
+    people.remove(victim)
+
+    # Tras ello, se elige a un atacante
+    attacker = choice(people)
+
+    message += attacker + choice(phrases) + victim
+    
+    if len(people) == 1:
+        message += '\n{} ha ganado el Battle Royale'.format(people[0])
+        print(message)
+        break
+    else:
+        message += '\nQuedan {} participantes vivos'.format(len(people))
+        print(message)
