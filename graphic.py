d8594db107f47cb2db7b2a661fd977b4e9722fd5
Piña <adanielpincab@gmail.com>
2019-07-11 19:32:02 +0200

    MANTENIMIENTO DE CÓDIGO Se ha documentado bien todo el código, además de eliminar restos de los tests hechos y convirtiendo el código, ahora sí, en uno completamente funcional.

--- graphic.py
+++ graphic.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont # librería pillow, para crear e interactuar con imágenes mediante Python.
 # Espacio para que quepa un nombre: 100 * 30 (aprox)
 
 EACH_ROW = 20 # numero de items por cada columna
@@ -40,11 +40,17 @@ def make_a_list(dict, name):
 	'''
 	Representa los items de una lista de python en una imagen
 	'''
+	# Se empieza con un ancho y alto de imagen nulo.
 	width = 0
 	height = 0
 
+	# Para que cualquier palabra quepa en cada columna, se establece como estandar
+	# un ancho suficiente como para que quepa la palabra más larga de la lista.
 	standard_width = 10*longest(dict)
 
+	# A continuación se calcula un ancho total de la imagen, que dependerá
+	# del número de items de la lista. (Cuando el numero de items exceda el máximo 
+	# del número de columnas actual [EACH_ROW], se añadirá una columna más) 
 	if length(dict) <= EACH_ROW:
 		width = standard_width
 		height  = 20*length(dict)
@@ -52,21 +58,33 @@ def make_a_list(dict, name):
 		width += standard_width * columns(dict)
 		height = EACH_ROW*20
 
+	# Se crean los elementos necesarios para crear e interactuar con la nueva imagen
+	# que vamos a crear (librería pillow)
 	img = Image.new('RGB', (width, height), color = (255, 255, 255))
 	fnt = ImageFont.truetype('media/Roboto-Regular.ttf', 15)
 	d = ImageDraw.Draw(img)
 
-	count = 0
+	# Tras tener la imagen el tamaño esperado, uno a uno se va añadiendo cada nombre.
+	# Cuando el jugador al que pertenezca el numbre ya haya muerto, el color de su nombre será gris y
+	# el nombre aparecerá tachado. Si no, aparece en negro y sin tachar.
+
+	count = 0# Esta variable permite numerar cada item.
 	for key in dict:
-		if dict[key]['live']:
-			color = (0, 0, 0)
-		else:
-			color = (128, 128, 128)
-		coord1 = 3+(standard_width+1)*int(count/EACH_ROW)
-		coord2 = (20*(count%EACH_ROW))
+		if dict[key]['live']:# Si está vivo
+			color = (0, 0, 0)# su color es negro
+		else:# si no...
+			color = (128, 128, 128)# será gris.
+
+		coord1 = 3+(standard_width+1)*int(count/EACH_ROW)# Primera coordenada, que dependerá del numero de columna al que pertenezca su numeración
+		coord2 = (20*(count%EACH_ROW))# La segunda coordenada, que esta vez dependerá de su posición vertical en la columna. 
+
+		# Una vez preparado todo, se coloca el texto en la posición calculada y con el color al que pertenece.
 		d.text((coord1 , coord2), key, font=fnt, fill=color)
+		
+		# Además, si no está vivo, el nombre será tachado. 
 		if not dict[key]['live']:
 			d.line((coord1,coord2+10, coord1+8*len(key),coord2+10), fill=128)
-		count += 1
+		
+		count += 1 # Se añade un número al contador, ya que nos disponemos a pasar al siguiente ítem.
 
-	img.save(name)
+	img.save(name)# Al terminar todo esto, se guarda la nueva imagen (al tener siempre el mismo nombre, sobreescribirá al ultimo guardado.)
--- main.py
+++ main.py
@@ -5,10 +5,6 @@
 '''
 #-----------------------------------------------------------------------
 
-'''TODO: cambiar a la lista de grupo de participantes de array a dictionary. 
-TODO: REPRESENTAR A CADA PARTICIPANTE COMO UN OBJETO, con característica vivo = True o False. (todo esto es para que 
-la parte de la generación de la lista en imagen pueda saber cuales estann muertos para tacharlos o algo o yo que se ponte las pilas macho'''
-
 # Rellenar esto con las claves de acceso ÚNICAS DE TU BOT
 API_KEY = ''
 API_SECRET = ''
@@ -30,17 +26,19 @@ with open('custom/PEOPLE.txt', 'r') as f:
     names = [i.rstrip('\n') for i in names] # Crea una lista de los participantes manejable por Python a partir del documento de texto
     f.close()
 
+with open('custom/PHRASES.txt', 'r') as f:
+    phrases = f.readlines()
+    phrases = [i.rstrip('\n') for i in phrases]# Lo mismo para las frases
+    f.close()
+
+# Se crea el objeto people, que contiene a todos los participantes junto a
+# su estado (vivos o muertos) y su número de bajas
 people = {}
 for i in names:
     people[i] = {
         'live':True,
         'kills':0
     }
-
-with open('custom/PHRASES.txt', 'r') as f:
-    phrases = f.readlines()
-    phrases = [i.rstrip('\n') for i in phrases]# Lo mismo para las frases
-    f.close()
 
 auth = tweepy.OAuthHandler(API_KEY, API_SECRET)# Autentificación
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)# Acceso
@@ -54,7 +52,7 @@ def max_kills(dict):
     '''
     Devuelve el o los concursantes con mayor numero de bajas
     '''
-    p_max = []
+    p_max = []# lista de jugadores con mas bajas
     maximum = 0
     for key in dict:
         if maximum < dict[key]['kills']:
@@ -62,26 +60,28 @@ def max_kills(dict):
             p_max = [key]
         elif maximum == dict[key]['kills']:
             p_max.append(key)
-    return p_max
+    return p_max # Devuelve un array con el jugador con más bajas, o con los jugadores con mas bajas en caso de haber empate.
 
 while True:
-    message = ''
+    message = '' # Mensaje que se enviará como tweet
 
     # Primero, se elige a una víctima y se elimina del grupo de participantes
     victim = choice(names)
-    people[victim]['live'] = False
+    people[victim]['live'] = False # al no estar ya viva, su estado en el objeto 'people' es de muerta.
     names.remove(victim)
 
     # Tras ello, se elige a un atacante
     attacker = choice(names)
-    people[attacker]['kills'] += 1
+    people[attacker]['kills'] += 1 # Al ser este el asesino, su numero de bajas aumenta por uno. 
 
     # Se crea el mensaje que lo comunica...
     message += attacker + choice(phrases) + victim
     
-    # ... y dependiendo de si aún queda suficiente gente como para seguir, se añade el numero de participantes restantes o el ganador
-    if len(names) == 1:
-        message += '\n{} ha ganado el Battle Royale'.format(names[0])
+    if len(names) == 1:# Si ya solo queda uno
+        message += '\n{} ha ganado el Battle Royale'.format(names[0])# Esa persona gana (se comunica su victoria)
+        
+        # Junto a quién ha ganado el battle royale, se comunica también quienes son los que han tenido el
+        # mayor número de bajas.
         if len(max_kills(people)) > 1:
             verb = 'han'
         else:
@@ -89,17 +89,16 @@ while True:
         message += '\n{0} {1} tenido el mayor numero de bajas, con un total de {2} asesinatos.'.format(' y '.join(max_kills(people)), verb, people[max_kills(people)[0]]['kills'])
 
         print(message)
-        make_a_list(people, 'media/lasting.png')
-        sleep(5)
- #       api.update_with_media('media/lasting.png', status=message)
-        break
-    else:
-        message += '\nQuedan {} participantes vivos'.format(len(names))
-        make_a_list(people, 'media/lasting.png')
-#        sleep(5)
-#        api.update_with_media('media/lasting.png', status=message)
+        make_a_list(people, 'media/lasting.png')# Se hace una lista actualizada de los concursantes (ver graphics.py)
+        sleep(5)# Se espera 5 segundos (tiempo de sobra para que la imagen se actualize)
+        api.update_with_media('media/lasting.png', status=message)# Se envía el nuevo tweet junto a la imagen de lista actualizada.
+        break # Se acaba el juego
+    else:# Si, en cambio, queda suficiente gente para seguir jugando
+        message += '\nQuedan {} participantes vivos'.format(len(names))# Se añade al mensaje el numero de participantes restantes
+        make_a_list(people, 'media/lasting.png')# Se hace una lista actualizada de los concursantes (ver graphics.py)
+        sleep(5)# Se espera 5 segundos (tiempo de sobra para que la imagen se actualize)
+        api.update_with_media('media/lasting.png', status=message)# Se envía el nuevo tweet junto a la imagen de lista actualizada.
         print(message)
 
     # Tras esto, se espera el tiempo establecido hasta el siguiente Tweet.
-#    sleep(DELAY)
-    input()
+    sleep(DELAY)
